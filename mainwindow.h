eb4c233d64c6e5acc8d2f1e3df1b30a645554307
Jacob <60986577+KuKaaa@users.noreply.github.com>
2020-05-09 13:04:08 +0200

    Created bunch of function declarations

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QList>
+
+//Widget for drawing
+class ScribbleArea;
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -13,9 +17,59 @@ class MainWindow : public QMainWindow
 
 public:
     MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    ~MainWindow() override;
+
+protected:
+    /* stops events from executing*/
+    void closeEvent(QCloseEvent *event) override;
+
+private slots:
+    /* open image file */
+    void open();
+
+    /* save image file */
+    void save();
+
+    /* changes pencil color */
+    void pencilColor();
+
+    /* changes pencil width */
+    void pencilWidth();
+
+    void about();
 
 private:
+    /* creates actions */
+    void createActions();
+
+    /* creates menu system*/
+    void createMenu();
+
+    /* save if changes occur */
+    bool maybeSave();
+
+    /* save file */
+    bool saveFile(const QByteArray &file);
+
+    /* paint area where draw */
+    ScribbleArea *scribbleArea;
+
+    QMenu *saveAsMenu;
+    QMenu *fileMenu;
+    QMenu *optionMenu;
+    QMenu *helpMenu;
+
+    QAction *openAction;
+
+    QList<QAction *> saveAsAction;
+
+    QAction *pencilColorAction;
+    QAction *pencilWidthAction;
+    QAction *printAction;
+    QAction *clsAction;
+    QAction *aboutQtAction;
+
     Ui::MainWindow *ui;
+
 };
 #endif // MAINWINDOW_H

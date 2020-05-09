976b03476e09d9cf07e5f60b92621ccdada32ab9
Jacob <60986577+KuKaaa@users.noreply.github.com>
2020-05-09 13:07:08 +0200

    Created class ScribbleArea and added some methods

---
+++ scribblearea.h
@@ -0,0 +1,56 @@
+#ifndef SCRIBBLEAREA_H
+#define SCRIBBLEAREA_H
+
+#include <QColor>
+#include <QImage>
+#include <QPoint>
+#include <QWidget>
+
+class ScribbleArea : public QWidget
+{
+    Q_OBJECT
+
+public:
+    ScribbleArea(QWidget *parent);
+    /* sets pencil color, e.g. red(#FF0000) */
+    void setPenColor(const QColor &newColor);
+    /* sets pencil width */
+    void setPenWidth(int newWidth);
+    /* open image file */
+    bool openImg(const QString &fileName);
+    /* save img file */
+    bool saveImg(const QString &fileName, const char *fileFormat);
+
+    bool isModified() const {return modified;}
+
+    QColor penColor() const {return myPenColor;}
+
+    int penWidth() const {return myPenWidth;}
+
+private slots:
+    void clearImg();
+    void printImg();
+
+protected:
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+
+    void paintEvent(QPaintEvent *event) override;
+    void resizeEvent(QResizeEvent *event) override;
+
+private:
+    void drawLineTo(const QPoint &endPoint);
+    void resizeImg(QImage *image, const QSize &newSize);
+
+    bool modified;
+    bool scribbling;
+
+    QColor myPenColor;
+    QImage image;
+    QPoint lastPoint;
+
+    int myPenWidth;
+
+};
+#endif // SCRIBBLEAREA_H

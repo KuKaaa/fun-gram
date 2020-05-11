c02112a1b32ddff5b1bd2b400bf419b998b43f08
Jacob <60986577+KuKaaa@users.noreply.github.com>
2020-05-11 13:01:57 +0200

    Added new SLOTS to createActs method

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -44,9 +44,7 @@ void MainWindow::open()
     {
         QString fileName = QFileDialog::getOpenFileName(this, tr("Open File"), QDir::currentPath());
         if(!fileName.isEmpty())
-        {
             scribbleArea->openImg(fileName);
-        }
     }
 }
 
@@ -54,26 +52,26 @@ void MainWindow::save()
 {
     QAction *action = qobject_cast<QAction *>(sender());
     QByteArray fileFormat = action->data().toByteArray();
+
     saveFile(fileFormat);
 }
 
 void MainWindow::penColor()
 {
     QColor newColor = QColorDialog::getColor(scribbleArea->penColor());
+
     if(newColor.isValid())
-    {
         scribbleArea->setPenColor(newColor);
-    }
 }       
 
 void MainWindow::penWidth()
 {
     bool ok;
-    int newWidth = QInputDialog::getInt(this, tr("Scribble"), tr("Select pen width : "), scribbleArea->penWidth(), 1, 50, 1, &ok);
+
+    int newWidth = QInputDialog::getInt(this, tr("Pen Width"), tr("Select your pen width : "), scribbleArea->penWidth(), 1, 50, 1, &ok);
+
     if(ok)
-    {
         scribbleArea->setPenWidth(newWidth);
-    }
 }
 
 void MainWindow::about()
@@ -85,41 +83,45 @@ void MainWindow::createActs()
 {
     openAct = new QAction(tr("&Open"), this);
     openAct->setShortcuts(QKeySequence::Open);
-    connect(openAct, SIGNAL(triggered()), this, SLOT(open()));
+    connect(openAct, SIGNAL(triggered()), this, SLOT(openAct()));
+
     foreach(QByteArray format, QImageWriter::supportedImageFormats())
     {
         QString text = tr("%1...").arg(QString(format).toUpper());
         QAction *action = new QAction(text, this);
         action->setData(format);
-        connect(action, SIGNAL(triggered()), scribbleArea, SLOT(print()));
+        connect(action, SIGNAL(triggered()), scribbleArea, SLOT(printAct()));
         saveAsActs.append(action);
     }
 
     printAct = new QAction(tr("&Print..."), this);
-    connect(printAct, SIGNAL(triggered()), scribbleArea, SLOT(print()));
-    exitAct = new QAction(tr("E&xit"), this);
+    connect(printAct, SIGNAL(triggered()), scribbleArea, SLOT(printAct()));
+
+    exitAct = new QAction(tr("&Exit"), this);
     exitAct->setShortcuts(QKeySequence::Quit);
-    connect(exitAct, SIGNAL(triggeres()), this, SLOT(print()));
+    connect(exitAct, SIGNAL(triggeres()), this, SLOT(exitAct()));
     
     penColorAct = new QAction(tr("&Pen Color..."), this);
     connect(penColorAct, SIGNAL(triggered()), this, SLOT(penColor()));
-    penWidthAct = new QAction(tr("Pen &Width..."), this);
+
+    penWidthAct = new QAction(tr("&Pen Width..."), this);
     connect(penWidthAct, SIGNAL(triggered()), this, SLOT(penWidth()));
     
-    clsAct = new QAction(tr("&Clear Screen"), this);
-    clsAct->setShortcut(tr("Ctrl+L"));
-    connect(clsAct, SIGNAL(triggered()), scribbleArea, SLOT(clearImage()));
+    clsAct = new QAction(tr("&Cls"), this);
+    clsAct->setShortcut(tr("Ctrl+C"));
+    connect(clsAct, SIGNAL(triggered()), scribbleArea, SLOT(clearImg()));
     
     aboutAct = new QAction(tr("&About..."), this);
-    connect(aboutAct, SIGNAL(triggered()), scribbleArea, SLOT(about()));
+    connect(aboutAct, SIGNAL(triggered()), scribbleArea, SLOT(aboutAct()));
     
-    aboutQtAct = new QAction(tr("&About &Qt..."), this);
-    connect(aboutQtAct, SIGNAL(triggered()), qApp, SLOT(about()));    
+    aboutQtAct = new QAction(tr("&About Qt..."), this);
+    connect(aboutQtAct, SIGNAL(triggered()), qApp, SLOT(aboutQtAct()));
 }
 
 void MainWindow::createMenu()
 {
     saveAsMenu = new QMenu(tr("&Save As"), this);
+
     foreach(QAction *action, saveAsActs)
     {
         saveAsMenu->addAction(action);
@@ -131,6 +133,7 @@ void MainWindow::createMenu()
     fileMenu->addAction(printAct);
     fileMenu->addSeparator();
     fileMenu->addAction(exitAct);
+
     optionMenu = new QMenu(tr("&Options"), this);
     optionMenu->addAction(penColorAct);
     optionMenu->addAction(penWidthAct);
@@ -154,26 +157,26 @@ bool MainWindow::maybeSave(){
                                    tr("This image has been modified.\n"
                                     "Do you want to save your changes?"),
                                    QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
+
         if(ret == QMessageBox::Save)
-        {
             return saveFile("png");
-        }
         else if (ret == QMessageBox::Cancel)
-        {
             return false;
-        }
     }
+
     return true;
 }
 
 bool MainWindow::saveFile(const QByteArray &fileFormat)
 {
-    QString initialPath = QDir::currentPath() + "/untilted." + fileFormat;
+    QString initialPath = QDir::currentPath() + "/*." + fileFormat;
+
     QString fileName = QFileDialog::getSaveFileName(this, tr("Save As"),
         initialPath,
         tr("%1 Files (*,%2);; All Files(*)")
         .arg(QString::fromLatin1(fileFormat.toUpper()))
         .arg(QString::fromLatin1(fileFormat)));
+
     if (fileName.isEmpty())
     {
         return false;
@@ -183,29 +186,3 @@ bool MainWindow::saveFile(const QByteArray &fileFormat)
         return scribbleArea->saveImg(fileName, fileFormat.constData());
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

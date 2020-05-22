cb5d4547a2d1eeb7719ffe59048f31c213ba5846
Jacob <60986577+KuKaaa@users.noreply.github.com>
2020-05-22 22:40:01 +0200

    Added body of filtering methods
    Added body for gauss blur method
    Added body for laplacian edge detection method

--- filterwindow.cpp
+++ filterwindow.cpp
@@ -5,6 +5,8 @@
 
 #include <QDesktopServices>
 #include <QMessageBox>
+#include <QMatrix3x3>
+#include <cmath>
 
 QImage filteredImg;
 QLabel* label;
@@ -19,7 +21,7 @@ FilterWindow::FilterWindow(QWidget *parent) :
 
     QIcon icon("");
     this->setWindowIcon(icon);
-    this->setMinimumSize(1280, 720);
+    this->setMinimumSize(1600, 900);
 
 }
 
@@ -76,20 +78,105 @@ void FilterWindow::on_action_Quit_triggered()
 void FilterWindow::on_action_Gauss_Blur_triggered()
 {
 
+    QImage img = ui->label->pixmap()->toImage();
+
+    QMatrix3x3 gaussKernel;
+    gaussKernel(0, 0) = 1;  gaussKernel(0, 1) = 2;  gaussKernel(0, 2) = 1;
+    gaussKernel(1, 0) = 2;  gaussKernel(1, 1) = 4;  gaussKernel(1, 2) = 2;
+    gaussKernel(2, 0) = 1;  gaussKernel(2, 1) = 2;  gaussKernel(2, 2) = 1;
+
+    float sum = 16.0;
+
+    for(int i = 0 ; i<img.width()-1; i++)
+    {
+        for(int j = 0 ; j<img.height()-1; j++)
+        {
+            float red = 0, green = 0, blue = 0;
+
+            red =
+                    gaussKernel(0, 0) * qRed(img.pixel(i+1, j+1)) + gaussKernel(0, 1) * qRed(img.pixel(i, j+1)) + gaussKernel(0, 2) * qRed(img.pixel(i-1, j+1)) +
+                    gaussKernel(1, 0) * qRed(img.pixel(i+1, j)) + gaussKernel(1, 1) * qRed(img.pixel(i, j)) + gaussKernel(1, 2) * qRed(img.pixel(i-1, j)) +
+                    gaussKernel(2, 0) * qRed(img.pixel(i+1, j-1)) + gaussKernel(2, 1) * qRed(img.pixel(i, j-1)) + gaussKernel(2, 2) * qRed(img.pixel(i-1, j-1));
+
+            green =
+                    gaussKernel(0, 0) * qGreen(img.pixel(i+1, j+1)) + gaussKernel(0, 1) * qGreen(img.pixel(i, j+1)) + gaussKernel(0, 2) * qGreen(img.pixel(i-1, j+1)) +
+                    gaussKernel(1, 0) * qGreen(img.pixel(i+1, j)) + gaussKernel(1, 1) * qGreen(img.pixel(i, j)) + gaussKernel(1, 2) * qGreen(img.pixel(i-1, j)) +
+                    gaussKernel(2, 0) * qGreen(img.pixel(i+1, j-1)) + gaussKernel(2, 1) * qGreen(img.pixel(i, j-1)) + gaussKernel(2, 2) * qGreen(img.pixel(i-1, j-1));
+
+            blue =
+                    gaussKernel(0, 0) * qBlue(img.pixel(i+1, j+1)) + gaussKernel(0, 1) * qBlue(img.pixel(i, j+1)) + gaussKernel(0, 2) * qBlue(img.pixel(i-1, j+1)) +
+                    gaussKernel(1, 0) * qBlue(img.pixel(i+1, j)) + gaussKernel(1, 1) * qBlue(img.pixel(i, j)) + gaussKernel(1, 2) * qBlue(img.pixel(i-1, j)) +
+                    gaussKernel(2, 0) * qBlue(img.pixel(i+1, j-1)) + gaussKernel(2, 1) * qBlue(img.pixel(i, j-1)) + gaussKernel(2, 2) * qBlue(img.pixel(i-1, j-1));
+
+            img.setPixel(i, j, qRgb(red/sum, green/sum, blue/sum));
+        }
+    }
+
+    ui->label->setPixmap(QPixmap::fromImage(img));
+
 }
 
 void FilterWindow::on_action_Laplacian_Edge_Detection_triggered()
 {
+
+    QImage img = ui->label->pixmap()->toImage();
+
+    QMatrix3x3 lapKernel;
+    lapKernel(0, 0) = -1;    lapKernel(0, 1) = -1;    lapKernel(0, 2) = -1;
+    lapKernel(1, 0) = -1;    lapKernel(1, 1) = 8;    lapKernel(1, 2) = -1;
+    lapKernel(2, 0) = -1;    lapKernel(2, 1) = -1;    lapKernel(2, 2) = -1;
+
+
+    for(int i = 0 ; i<img.width()-1; i++)
+    {
+        for(int j = 0 ; j<img.height()-1; j++)
+        {
+            float red = 0, green = 0, blue = 0;
+
+            red =
+                    lapKernel(0, 0) * qRed(img.pixel(i+1, j+1)) + lapKernel(0, 1) * qRed(img.pixel(i, j+1)) + lapKernel(0, 2) * qRed(img.pixel(i-1, j+1)) +
+                    lapKernel(1, 0) * qRed(img.pixel(i+1, j)) + lapKernel(1, 1) * qRed(img.pixel(i, j)) + lapKernel(1, 2) * qRed(img.pixel(i-1, j)) +
+                    lapKernel(2, 0) * qRed(img.pixel(i+1, j-1)) + lapKernel(2, 1) * qRed(img.pixel(i, j-1)) + lapKernel(2, 2) * qRed(img.pixel(i-1, j-1));
+
+            green =
+                    lapKernel(0, 0) * qGreen(img.pixel(i+1, j+1)) + lapKernel(0, 1) * qGreen(img.pixel(i, j+1)) + lapKernel(0, 2) * qGreen(img.pixel(i-1, j+1)) +
+                    lapKernel(1, 0) * qGreen(img.pixel(i+1, j)) + lapKernel(1, 1) * qGreen(img.pixel(i, j)) + lapKernel(1, 2) * qGreen(img.pixel(i-1, j)) +
+                    lapKernel(2, 0) * qGreen(img.pixel(i+1, j-1)) + lapKernel(2, 1) * qGreen(img.pixel(i, j-1)) + lapKernel(2, 2) * qGreen(img.pixel(i-1, j-1));
+
+            blue =
+                    lapKernel(0, 0) * qBlue(img.pixel(i+1, j+1)) + lapKernel(0, 1) * qBlue(img.pixel(i, j+1)) + lapKernel(0, 2) * qBlue(img.pixel(i-1, j+1)) +
+                    lapKernel(1, 0) * qBlue(img.pixel(i+1, j)) + lapKernel(1, 1) * qBlue(img.pixel(i, j)) + lapKernel(1, 2) * qBlue(img.pixel(i-1, j)) +
+                    lapKernel(2, 0) * qBlue(img.pixel(i+1, j-1)) + lapKernel(2, 1) * qBlue(img.pixel(i, j-1)) + lapKernel(2, 2) * qBlue(img.pixel(i-1, j-1));
+
+            img.setPixel(i, j, qRgb(abs(red), abs(green), abs(blue)));
+        }
+    }
+
+    ui->label->setPixmap(QPixmap::fromImage(img));
 
 }
 
 void FilterWindow::on_action_Gradient_Directional_Filter_triggered()
 {
 
+    QImage img = ui->label->pixmap()->toImage();
+
+    QMatrix3x3 gradientKernel;
+    gradientKernel(0, 0) = -1;    gradientKernel(0, 1) = 1;    gradientKernel(0, 2) = 1;
+    gradientKernel(1, 0) = -1;    gradientKernel(1, 1) = -2;    gradientKernel(1, 2) = 1;
+    gradientKernel(2, 0) = -1;    gradientKernel(2, 1) = 1;    gradientKernel(2, 2) = 1;
+
 }
 
 void FilterWindow::on_action_Outline_Filter_triggered()
 {
+
+    QImage img = ui->label->pixmap()->toImage();
+
+    QMatrix3x3 outlineKernel;
+    outlineKernel(0, 0) = 1;    outlineKernel(0, 1) = 2;    outlineKernel(0, 2) = 1;
+    outlineKernel(1, 0) = 0;    outlineKernel(1, 1) = 0;    outlineKernel(1, 2) = 0;
+    outlineKernel(2, 0) = -1;    outlineKernel(2, 1) = -2;    outlineKernel(2, 2) = -1;
 
 }
 
